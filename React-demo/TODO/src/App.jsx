674ea2436c89f411c34ddb5f37a0902328844639
nydo(mac) <2337315348@qq.com>
2025-10-10 23:59:51 +1100

    现在可以通过遍历模拟数据库, 把数据库内的todo 项全部渲染到页面中作为 li 元素

--- React-demo/TODO/src/App.jsx
+++ React-demo/TODO/src/App.jsx
@@ -1,29 +1,34 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
+// 模拟数据库
+const DATA = [
+  {
+    id: 1,
+    title: 'todo1',
+    completed: false,
+  },
+  {
+    id: 2,
+    title: 'todo2',
+    completed: true,
+  },
+  {
+    id: 3,
+    title: 'todo3',
+    completed: false,
+  },
+];
+
 function App() {
-  const [count, setCount] = useState(0);
+  // todo MVP, 能够渲染数据库内的所有 todo 项
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <ul>
+        {DATA.map((todo) => {
+          return <li key={todo.id}>{todo.title}</li>;
+        })}
+      </ul>
     </>
   );
 }
